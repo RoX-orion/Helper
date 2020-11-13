dc9b81ceb4e22316c98c5c9a140071a08c0cda4d
RoX-orion <528289176@qq.com>
2020-11-13 08:24:59 +0800

    init

---
+++ src/views/test/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p>ncahsb</p>
+  </div>
+</template>
---
+++ vue.config.js
@@ -0,0 +1,31 @@
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+const port = process.env.port || process.env.npm_config_port || 9999 // dev port
+
+module.exports = {
+  publicPath: '/',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  lintOnSave: false,//process.env.NODE_ENV === 'development',
+  productionSourceMap: false,
+  devServer: {
+    port: port,
+    // open: true,
+    overlay: {
+      warnings: false,
+      errors: true
+    }
+    // ,
+    // proxy: {
+    //   [process.env.VUE_APP_BASE_API]: {
+    //     target: 'http://localhost:8888',
+    //     changOrigin: true,
+    //     pathRewrite: {
+    //       ['^' + process.env.VUE_APP_BASE_API]: ''
+    //     }
+    //   }
+    // }
+  }
+}
